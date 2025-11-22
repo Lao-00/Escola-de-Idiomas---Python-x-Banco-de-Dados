1a4b23af5b1be9e336073e15c8dcede9ce0eb8be
Lao-00 <leocsgmvasconcelos@gmail.com>
2025-11-22 03:14:52 -0300

    Atulização do projeto

--- Escola de Idiomas - Python x BD/services/professor_services.py
+++ Escola de Idiomas - Python x BD/services/professor_services.py
@@ -6,7 +6,7 @@ def cadastrar_professor(nome_professor: str, especialidade_professor: str, cpf_p
         con = criar_conexao()
         cursor = con.cursor()
 
-        sql_usuario = "INSERT INTO Usuario (email, senha) VALUES (%s, %s) RETURNING usuario_id;"
+        sql_usuario = "INSERT INTO usuario (email, senha) VALUES (%s, %s) RETURNING usuario_id;"
         cursor.execute(sql_usuario, (email, senha))
         usuario_id = cursor.fetchone()[0]
 
@@ -21,4 +21,37 @@ def cadastrar_professor(nome_professor: str, especialidade_professor: str, cpf_p
         cursor.close()
         con.close()
 
+def remover_professor(professor_id):
 
+    try:
+        con = criar_conexao()
+        cursor = con.cursor()
+
+        sql = 'DELETE FROM professor WHERE professor_id = %s'
+        cursor.execute(sql, (professor_id))
+        con.commit()
+        print("Professor removido!")
+        
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        con.close()
+
+def update_professor(usuario_id, professor_id):
+
+    try:
+        con = criar_conexao()
+        cursor = con.cursor()
+
+
+        sql = 'UPDATE professor SET usuario_id = %s WHERE professor_id = %s'
+        cursor.execute(sql, (usuario_id, professor_id))
+        con.commit()
+        print("Dados do professor atualizados!")
+        
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        con.close()
--- Escola de Idiomas - Python x BD/services/turma_services.py
+++ Escola de Idiomas - Python x BD/services/turma_services.py
@@ -1,15 +1,87 @@
 from config.db import criar_conexao
+#from services.curso_services import cadastrar_curso
 
-def cadastrar_turma(horario_turma):
+def cadastrar_turma(horario_turma: str, curso_id: int):
+    
+    try:
+        con = criar_conexao()
+        cursor = con.cursor()
+
+        sql = "INSERT INTO turma(horario_turma, curso_id) VALUES(%s, %s) RETURNING turma_id"
+        cursor.execute(sql, (horario_turma, curso_id))
+        turma_id = cursor.fetchone()
+        con.commit()
+        print("Turma cadastrada com sucesso!")
+
+
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        con.close()
+
+def remover_turma(turma_id):
 
     try:
         con = criar_conexao()
         cursor = con.cursor()
 
-        sql = "INSERT INTO turma(horario_curso) VALUES(%s)"
-        cursor.execute(sql, (horario_turma))
+        sql = 'DELETE FROM turma WHERE turma_id = %s'
+        cursor.execute(sql, (turma_id))
         con.commit()
-        print("Turma cadastrada com sucesso!")
+        print("Turma removida!")
+        
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        con.close()
+
+def update_turma(horario_turma, turma_id):
+
+    try:
+        con = criar_conexao()
+        cursor = con.cursor()
+
+
+        sql = 'UPDATE turma SET horario_turma = %s WHERE turma_id = %s'
+        cursor.execute(sql, (horario_turma, turma_id))
+        con.commit()
+        print("Dados da turma atualizados!")
+        
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        con.close()
+
+def adicionar_aluno(aluno_id, turma_id):
+
+    try:
+        con = criar_conexao()
+        cursor = con.cursor()
+
+        sql = 'UPDATE turma SET aluno_id = %s WHERE turma_id = %s'
+        cursor.execute(sql, (aluno_id, turma_id))
+        con.commit()
+        print("Aluno matriculado na turma com sucesso!")
+
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        con.close()
+
+def adicionar_professor(professor_id, turma_id,):
+
+    try:
+        con = criar_conexao()
+        cursor = con.cursor()
+
+        sql = 'UPDATE turma SET professor_id = %s WHERE turma_id = %s'
+        cursor.execute(sql, (professor_id, turma_id))
+        con.commit()
+        print("Professor registrado com sucesso!")
 
     except Exception as e:
         print(e)
